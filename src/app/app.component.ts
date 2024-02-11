55bd55c422ed38f5009d321c05b0e154f3064e2c
Zoltán Herwerth <herwerth@users.noreply.github.com>
2024-02-11 18:16:19 +0100

    Add-element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sablon';
+  elements = [
+    "Lorem ipsum dolor",
+    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
+    "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  ];
+
+  onSubmit(e:any){
+    e.preventDefault();
+    const newValue = e.target.elements.title.value;
+    this.elements.push(newValue);
+    e.target.reset();
+  }
+  elementDelete(i: number){
+    this.elements.splice(i,1);
+
+  }
 }
